929317c27804a2f158dae5b36fd5b8aba9846fd3
Kayo Nakao <kayo.nakao87@gmail.com>
2025-04-17 18:02:35 -0400

    Added created date

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import moment from "moment";
 
 const app = express();
 const port = 3000;
@@ -10,8 +11,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.post("/submit", (req, res) => {
+    var now = new Date();
+    var dateString = moment(now).format('LL');
+    req.body.date = dateString;
     posts.push(req.body);
-    res.render("index.ejs", {title: req.body.title, content: req.body.text, posts: posts});
+    res.render("index.ejs", {title: req.body.title, content: req.body.text, date: dateString, posts: posts});
+});
+
+app.get("/post/:index", (req, res) => {
+    var post = posts[req.params.index];
+    res.render("index.ejs", {title: post.title, content: post.text, date: post.date, posts: posts});
 });
 
 app.get("/write", (req, res) => {
@@ -24,7 +33,7 @@ app.get("/", (req, res) => {
     } else {
         const index = posts.length - 1;
         const post = posts[index]
-        res.render("index.ejs", {title: post.title, content: post.text, posts: posts});
+        res.render("index.ejs", {title: post.title, content: post.text, date: post.date, posts: posts});
     }
 });
 
