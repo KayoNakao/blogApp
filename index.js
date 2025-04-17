ed70f7b41c59084ae2cb69ed58ad72214aed4d81
Kayo Nakao <kayo.nakao87@gmail.com>
2025-04-17 16:15:01 -0400

    Implemented write an article page

--- index.js
+++ index.js
@@ -4,11 +4,28 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+var posts = [];
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.post("/submit", (req, res) => {
+    posts.push(req.body);
+    res.render("index.ejs", {title: req.body.title, content: req.body.text, posts: posts});
+});
+
+app.get("/write", (req, res) => {
+    res.render("write.ejs");
+});
+
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    if (posts.length === 0) {
+        res.render("index.ejs");
+    } else {
+        const index = posts.length - 1;
+        const post = posts[index]
+        res.render("index.ejs", {title: post.title, content: post.text, posts: posts});
+    }
 });
 
 app.listen(port, () => {
